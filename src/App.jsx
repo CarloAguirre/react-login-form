7f8bedbd3fe0f190d64addaffafc5a384fa7d9b6
Carlo Aguirre <carlo_aguirre@outlook.cl>
2023-03-07 18:06:40 -0300

    correct mail and pass added to HTML

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
-import { useState } from 'react';
 import './App.css';
 
-// import  {AlertMsg}  from './components/AlertMsg';
 import LoginForm from './components/LoginFrom';
-
-
 
 export const App = () => {
 
-
-
-
   return (
     <>
-    
+    <p className='ms-2'>mail: desafio@latam.com, password: 123456</p>    
       <LoginForm /> 
     </>
   )
